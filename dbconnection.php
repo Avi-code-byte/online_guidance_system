f068f26c57326abf31a0890af3c012b54cf6cf16
Abhinav Mishra <69217002+Avi-code-byte@users.noreply.github.com>
2022-11-17 21:27:26 +0530

    uploading all files

---
+++ dbconnection.php
@@ -0,0 +1,18 @@
+
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database="careerguidance";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$database,3306);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
